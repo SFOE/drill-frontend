e4382bdb969660a07d1f61ef6f951938dcc309ea
rochette <olivier.monod@gmail.com>
2026-01-15 16:59:20 +0100

    add router to allow language selection using querystring

--- src/stores/languageStore.ts
+++ src/stores/languageStore.ts
@@ -1,4 +1,3 @@
-// stores/language.ts
 import { defineStore } from 'pinia'
 import { ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -7,13 +6,11 @@ export const useLanguageStore = defineStore('language', () => {
   const { locale } = useI18n()
   const currentLocale = ref(locale.value)
 
-  // Watch the store and update vue-i18n locale
   watch(currentLocale, (newLocale) => {
     locale.value = newLocale
     localStorage.setItem('app-locale', newLocale)
   })
 
-  // Load from localStorage if exists
   const savedLocale = localStorage.getItem('app-locale')
   if (savedLocale) {
     currentLocale.value = savedLocale
