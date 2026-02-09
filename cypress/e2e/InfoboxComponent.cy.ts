57885dddbdefd73fa0d0c5b2c52996a116d4188c
Zozo9k <zoe.jdpx@gmail.com>
2026-02-09 13:46:02 +0100

    cypress tests for infobox component

---
+++ cypress/e2e/InfoboxComponent.cy.ts
@@ -0,0 +1,212 @@
+/// <reference types="cypress" />
+import { mockDrillCategoryApi, mockBackendResponses } from '../support/mock-backend'
+
+describe('Infobox Component', () => {
+  beforeEach(() => {
+    // Load the app before each test
+    cy.visit('/')
+
+    // Wait for map to fully load - wait for canvas to have proper dimensions
+    cy.get('.ol-viewport canvas', { timeout: 50000 })
+      .should(($canvas) => {
+        const canvas = $canvas[0] as HTMLCanvasElement
+        expect(canvas.width).to.be.greaterThan(0)
+        expect(canvas.height).to.be.greaterThan(0)
+      })
+  })
+
+  it('does not render infobox when no data is selected', () => {
+    // Initially, without selecting a location, infobox should not be visible
+    cy.get('.info-box').should('not.exist')
+  })
+
+  it('renders infobox with icon and title', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('suitable')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    // Wait for infobox to appear
+    cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+    // Check that the icon is visible
+    cy.get('.info-box .icon img').should('exist').and('be.visible')
+
+    // Check that the title (h2) is visible
+    cy.get('.info-box .text h2').should('exist').and('be.visible')
+  })
+
+  // it('displays selected address when clicking map', () => {
+  //   // Mock backend to return suitable response
+  //   mockDrillCategoryApi('suitable')
+
+  //   // Trigger API call by clicking canvas
+  //   cy.get('.ol-viewport canvas').click('center')
+
+  //   // Wait for infobox to appear
+  //   cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+  //   // Check that the selected address is displayed
+  //   cy.get('.info-box .selected-address').should('exist').and('be.visible').and('have.text', 'L\'Auge-du-Bois 2b 2616 Renan BE')
+  // })
+
+  it('displays selected address when using search bar', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('suitable')
+
+    // Trigger API call by clicking canvas
+    cy.get('.input-wrapper input').type('L\'Auge-du-Bois 2b 2616 Renan BE{enter}')
+
+    // Wait for infobox to appear
+    cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+    // Check that the selected address is displayed
+    cy.get('.info-box .selected-address').should('exist').and('be.visible').and('have.text', 'L\'Auge-du-Bois 2b 2616 Renan BE')
+  })
+
+  it('infobox has correct color class for suitable (harmonized value = 1)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('suitable')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('green')
+    })
+  })
+
+  it('infobox has correct color class for with restrictions (harmonized value = 2)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('withRestrictions')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('orange')
+    })
+  })
+
+  it('infobox has correct color class for forbidden (harmonized value = 3)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('forbidden')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('red')
+    })
+  })
+
+  it('infobox has correct color class for unavailable (harmonized value = 4)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('unknown')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('blue')
+    })
+  })
+
+  it('infobox has correct color class for outside CH (harmonized value = 6)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('outOfSwitzerland')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('blue')
+    })
+  })
+
+  it('infobox has correct color class for error (harmonized value = 99)', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('error')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    //verify it has the right color class
+    cy.get('.info-box').then(($infobox) => {
+      expect($infobox.attr('class')).to.contain('purple')
+    })
+  })
+
+  it('shows expand button on mobile when infobox is rendered', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('suitable')
+
+    // Set mobile viewport
+    cy.viewport('iphone-x')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    // Wait for infobox to appear
+    cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+    // On mobile, there should be an expand button
+    cy.get('.info-box .expand-cta').should('exist').and('be.visible')
+  })
+
+  it('expands and collapses details on mobile when clicking expand button', () => {
+    // Mock backend to return suitable response
+    mockDrillCategoryApi('suitable')
+
+    // Set mobile viewport
+    cy.viewport('iphone-x')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    // Wait for infobox to appear
+    cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+    // Initially, details should be hidden
+    cy.get('.info-box .details').should('not.be.visible')
+
+    // Click expand button
+    cy.get('.info-box .expand-cta').click()
+
+    // Details should now be visible
+    cy.get('.info-box .details').should('be.visible')
+
+    // Click expand button again to collapse
+    cy.get('.info-box .expand-cta').click()
+
+    // Details should be hidden again
+    cy.get('.info-box .details').should('not.be.visible')
+  })
+
+
+  it('displays links when available', () => {
+    // Mock backend to return suitable response with links
+    mockDrillCategoryApi('suitable')
+
+    // Trigger API call by clicking canvas
+    cy.get('.ol-viewport canvas').click('center')
+
+    // Wait for infobox to appear
+    cy.get('.info-box', { timeout: 5000 }).should('exist')
+
+    cy.get('.info-box .expand-cta').click()
+
+    // Check if links container exists (it appears conditionally)
+    cy.get('.links-container').then(($sourceValues) => {
+      if ($sourceValues.length > 0) {
+        cy.wrap($sourceValues).should('be.visible')
+      }
+    })
+  })
+})
---
+++ cypress/support/mock-backend.ts
@@ -0,0 +1,174 @@
+/**
+ * Mock Backend Helper for Testing
+ * Use these utilities to intercept backend API calls and return mock responses
+ */
+
+export const mockBackendResponses = {
+  // Suitable for geothermal heating (harmonized_value: 1, green)
+  suitable: {
+    coord_x: 2683141,
+    coord_y: 1247500,
+    canton: 'BE',
+    canton_config: {
+      legend_url: 'https://wms.geo.admin.ch/?service=WMS&version=1.3.0&request=GetLegendGraphic&layers=ch.bfe.suitability-heat-pump&styles=&format=image/png',
+      cantonal_energy_service_url: 'https://www.bfe.admin.ch/',
+      thematic_geoportal_url: 'https://map.geo.admin.ch/'
+    },
+    ground_category: {
+      layer_results: [
+        {
+          layer: 'ch.bfe.suitability-heat-pump',
+          property_name: 'suitability',
+          value: '1'
+        }
+      ],
+      harmonized_value: 1,
+      source_values: 'ch.bfe.suitability-heat-pump'
+    },
+    result_detail: {
+      message: 'Geothermal heating is highly suitable',
+      full_url: 'https://www.bfe.admin.ch/',
+      detail: 'Ground conditions are ideal for heat pump installation'
+    }
+  },
+
+  // Conditionally suitable (harmonized_value: 2, orange)
+  withRestrictions: {
+    coord_x: 2683141,
+    coord_y: 1247500,
+    canton: 'BE',
+    canton_config: {
+      legend_url: 'https://wms.geo.admin.ch/?service=WMS&version=1.3.0&request=GetLegendGraphic&layers=ch.bfe.suitability-heat-pump&styles=&format=image/png',
+      cantonal_energy_service_url: 'https://www.bfe.admin.ch/',
+      thematic_geoportal_url: 'https://map.geo.admin.ch/'
+    },
+    ground_category: {
+      layer_results: [
+        {
+          layer: 'ch.bfe.suitability-heat-pump',
+          property_name: 'suitability',
+          value: '2'
+        }
+      ],
+      harmonized_value: 2,
+      source_values: 'ch.bfe.suitability-heat-pump'
+    },
+    result_detail: {
+      message: 'Geothermal heating is conditionally suitable',
+      full_url: 'https://www.bfe.admin.ch/',
+      detail: 'Ground conditions have some limitations'
+    }
+  },
+
+  // Not suitable (harmonized_value: 3, red)
+  forbidden: {
+    coord_x: 2683141,
+    coord_y: 1247500,
+    canton: 'BE',
+    canton_config: {
+      legend_url: 'https://wms.geo.admin.ch/?service=WMS&version=1.3.0&request=GetLegendGraphic&layers=ch.bfe.suitability-heat-pump&styles=&format=image/png',
+      cantonal_energy_service_url: 'https://www.bfe.admin.ch/',
+      thematic_geoportal_url: 'https://map.geo.admin.ch/'
+    },
+    ground_category: {
+      layer_results: [
+        {
+          layer: 'ch.bfe.suitability-heat-pump',
+          property_name: 'suitability',
+          value: '3'
+        }
+      ],
+      harmonized_value: 3,
+      source_values: 'ch.bfe.suitability-heat-pump'
+    },
+    result_detail: {
+      message: 'Geothermal heating is not suitable',
+      full_url: 'https://www.bfe.admin.ch/',
+      detail: 'Ground conditions do not allow installation'
+    }
+  },
+
+  // Unknown/unavailable (harmonized_value: 4, blue)
+  unknown: {
+    coord_x: 2683141,
+    coord_y: 1247500,
+    canton: 'BE',
+    canton_config: {
+      legend_url: 'https://wms.geo.admin.ch/?service=WMS&version=1.3.0&request=GetLegendGraphic&layers=ch.bfe.suitability-heat-pump&styles=&format=image/png',
+      cantonal_energy_service_url: 'https://www.bfe.admin.ch/',
+      thematic_geoportal_url: 'https://map.geo.admin.ch/'
+    },
+    ground_category: {
+      layer_results: [],
+      harmonized_value: 4,
+      source_values: ''
+    },
+    result_detail: {
+      message: 'Data unavailable',
+      full_url: '',
+      detail: 'Information is not available'
+    }
+  },
+
+  // Outside Switzerland (harmonized_value: 6, blue)
+  outOfSwitzerland: {
+    coord_x: 100,
+    coord_y: 100,
+    canton: null,
+    canton_config: null,
+    ground_category: {
+      layer_results: [],
+      harmonized_value: 6,
+      source_values: 'outside_switzerland'
+    },
+    result_detail: {
+      message: 'Location outside Switzerland',
+      full_url: '',
+      detail: 'This location is outside Switzerland'
+    }
+  },
+
+  // Server error (harmonized_value: 99, purple)
+  error: {
+    coord_x: 2683141,
+    coord_y: 1247500,
+    canton: null,
+    canton_config: null,
+    ground_category: {
+      layer_results: [],
+      harmonized_value: 99,
+      source_values: 'server_error'
+    },
+    result_detail: {
+      message: 'Error retrieving data',
+      full_url: '',
+      detail: 'An error occurred'
+    }
+  }
+}
+
+/**
+ * Mock the drill-category API endpoint
+ * Usage in tests:
+ *
+ * cy.intercept('GET', 'ADD TWO STARS HERE/v1/drill-category/**', {
+ *   statusCode: 200,
+ *   body: mockBackendResponses.suitable
+ * })
+ */
+export function mockDrillCategoryApi(responseType: keyof typeof mockBackendResponses) {
+  cy.intercept('GET', '**/v1/drill-category/**', {
+    statusCode: 200,
+    body: mockBackendResponses[responseType]
+  }).as('drillCategory')
+}
+
+/**
+ * Mock API error response
+ */
+export function mockDrillCategoryError() {
+  cy.intercept('GET', '**/v1/drill-category/**', {
+    statusCode: 500,
+    body: { error: 'Internal Server Error' }
+  }).as('drillCategoryError')
+}
