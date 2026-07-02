f538f521cb5b52a4b40b7b7b411f875003a2579b
GO-MOO <olivier.monod@gmail.com>
2026-07-02 08:03:25 +0200

    more tests

--- src/composables/__tests__/useDevice.test.ts
+++ src/composables/__tests__/useDevice.test.ts
@@ -46,8 +46,7 @@ describe('useDevice', () => {
     Object.defineProperty(window, 'innerWidth', { value: 1024, configurable: true })
     Object.defineProperty(navigator, 'maxTouchPoints', { value: 0, configurable: true })
     // Ensure ontouchstart is not defined
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    delete (window as any).ontouchstart
+    delete (window as Record<string, unknown>).ontouchstart
 
     const wrapper = mount(TestComponent)
     expect(wrapper.vm.isMobile).toBe(false)
@@ -66,8 +65,7 @@ describe('useDevice', () => {
   it('updates isMobile on window resize', async () => {
     Object.defineProperty(window, 'innerWidth', { value: 1024, configurable: true })
     Object.defineProperty(navigator, 'maxTouchPoints', { value: 0, configurable: true })
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    delete (window as any).ontouchstart
+    delete (window as Record<string, unknown>).ontouchstart
 
     const wrapper = mount(TestComponent)
     expect(wrapper.vm.isMobile).toBe(false)
